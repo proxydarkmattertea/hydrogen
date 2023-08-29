33d91524279132b1e2db28ffc891b9a095501a47
proxy <92996582+proxydarkmattertea@users.noreply.github.com>
2023-08-29 04:50:23 -0500

    Update 2023 from Discord.js v13 to v14
    Added command handler and separate commands via discord.js v14 documentation, planning to add music functionality back later.

--- index.js
+++ index.js
@@ -1,134 +1,55 @@
-// Require discord.js
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+// Require discord.js and init
+
+const fs = require('node:fs');
+const path = require('node:path')
+const { Client, Collection, Events, GatewayIntentBits, Partials} = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 const { token } = require('./config.json');
 
-// Client - Prefix
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]});
-const settings = {
-	prefix: '!'
-};
-// Music
-const { Player } = require("discord-music-player");
-const player = new Player(client, {
-	leaveOnEmpty: false,
-});
-client.player = player;
-
-client.on('messageCreate', async (message) => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift();
-    let guildQueue = client.player.getQueue(message.guild.id);
-
-    if(message.content.startsWith(prefix) && command === 'play') {
-        let queue = client.player.createQueue(message.guild.id);
-        await queue.join(message.member.voice.channel);
-        let song = await queue.play(args.join(' ')).catch(_ => {
-            if(!guildQueue)
-                queue.stop();
-        });
-    }
-
-    if(message.content.startsWith(prefix) && command === 'playlist') {
-        let queue = client.player.createQueue(message.guild.id);
-        await queue.join(message.member.voice.channel);
-        let song = await queue.playlist(args.join(' ')).catch(_ => {
-            if(!guildQueue)
-                queue.stop();
-        });
-    }
-
-    if(message.content.startsWith(prefix) && command === 'skip') {
-        guildQueue.skip();
-    }
-
-    if(message.content.startsWith(prefix) && command === 'stop') {
-        guildQueue.stop();
-    }
-
-    if(message.content.startsWith(prefix) && command === 'stoploop') {
-        guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
-    }
-
-    if(message.content.startsWith(prefix) && command === 'loopsong') {
-        guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
-    }
-
-    if(message.content.startsWith(prefix) && command === 'tloop') {
-        guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
-    }
-
-    if(message.content.startsWith(prefix) && command === 'seek') {
-        guildQueue.seek(parseInt(args[0]) * 1000);
-    }
-
-    if(message.content.startsWith(prefix) && command === 'clear') {
-        guildQueue.clearQueue();
-    }
-
-    if(message.content.startsWith(prefix) && command === 'shuffle') {
-        guildQueue.shuffle();
-    }
-
-    if(message.content.startsWith(prefix) && command === 'queue') {
-        console.log(guildQueue);
-    }
-
-    if(message.content.startsWith(prefix) && command === 'np') {
-        console.log(`Now playing: ${guildQueue.nowPlaying}`);
-        message.channel.send(`Now playing: ${guildQueue.nowPlaying}`);
-    }
-
-    if(message.content.startsWith(prefix) && command === 'pause') {
-        guildQueue.setPaused(true);
-    }
-
-    if(message.content.startsWith(prefix) && command === 'resume') {
-        guildQueue.setPaused(false);
-    }
-
-    if(message.content.startsWith(prefix) && command === 'remove') {
-        guildQueue.remove(parseInt(args[0]));
-    }
-
-    if(message.content.startsWith(prefix) && command === 'time') {
-        const ProgressBar = guildQueue.createProgressBar();
-
-        // [======>              ][00:35/2:20]
-        console.log(ProgressBar.prettier);
-    }
-})
-
-//Collections
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
-// Ready check
-client.once('ready', () => {
-    console.log('Ready!');
-});
+//Interaction
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
 
-// Slash Commands
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
 	try {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
+});
+
+// Ready check
+client.once('ready', () => {
+    console.log('Ready!');
 });
 
 // Login
